c9d7198ff4b6c7a9ebfe0648c0dedfeca0a73e58
the-cold-heart <93778898+the-cold-heart@users.noreply.github.com>
2026-05-09 17:29:09 +0800

    Update whitelist URLs
    Removed several URLs from the whitelist and added a new one.

--- src/white_list.ts
+++ src/white_list.ts
@@ -3,9 +3,5 @@ export default [
 	"http://127.0.0.1", // localhost
 	"http://localhost:5173", // localhost
 	"http://127.0.0.1:5173", // localhost
-	"https://glink25.github.io",
-	"https://oncent.github.io",
-	"https://cent.linkai.work",
-	"https://cent.link-ai.workers.dev",
-	"dailycent://"
+	"https://zd.lengxin.net"
 ];
